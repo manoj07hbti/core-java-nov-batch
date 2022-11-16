208c11f9889053ce78545dd3784f83c07b9ced61
lap90top <agalkagoyal@gmail.com>
2022-11-16 16:24:23 +0530

    This commit for feature branch creation

---
+++ src/dailyassignment/FeatureBranchCreation.java
@@ -0,0 +1,11 @@
+package dailyassignment;
+
+public class FeatureBranchCreation {
+
+    public static void main(String[] args) {
+
+       System.out.println("i have created a feature branch");
+
+    }
+
+}
