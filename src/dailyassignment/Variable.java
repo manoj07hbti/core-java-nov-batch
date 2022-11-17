8acca56284fb0c5a116dde9353619fa92808f114
lap90top <agalkagoyal@gmail.com>
2022-11-17 16:57:15 +0530

    this commit for the percentage and cube of the number

---
+++ src/dailyassignment/Variable.java
@@ -0,0 +1,42 @@
+package dailyassignment;
+
+public class Variable {
+
+    // main method
+    public static void main(String[] args) {
+
+
+
+        // Find out the percentage of the number
+
+       // Syntax for Variable : Datatype variable_name = value;
+
+       int number = 12493;   // variable_name 1------number
+       int percentage = 15; // variable_name 2 ------percentage
+       int result  = (number*percentage)/100; // variable_name 3--------result
+
+       System.out.println("Percentage of the number is:"+result); //output 1873
+
+
+       // Find out the cube of the number(12493)
+
+
+        int cube = number*number*number; //variable_name 4 ------cube
+        System.out.println("Cube of 12493 is:"+cube); // cube of 12493 is :1949845587157
+        // 'but' it is printing the cube value is: -69565227
+
+
+
+        // Find out the cube of 10
+        int number1 = 10;
+        int cube1 = number1*number1*number1;
+        System.out.println("Cube of 10 is:"+cube1); // output 1000
+
+
+
+
+    }
+
+
+
+}
