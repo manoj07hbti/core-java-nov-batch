42568fc725dfd510bdd5bfae8cae016e0171a940
Gaurang yadav <yadavgaurang11@gmail.com>
2022-11-17 16:26:38 +0530

    git testing

--- src/git/percentage.java
+++ src/git/percentage.java
@@ -6,6 +6,9 @@ public class percentage {
      int number=20000;
     double result=(number*percentage)/100;
      System.out.println(result);
+     int num=5;
+     int cube=(num*num*num);
+      System.out.println(cube);
 
 
 
