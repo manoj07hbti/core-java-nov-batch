9e7cac15c9bd353748057261251ae6f1ed608a6b
DEVESH KUMAR <Deep789516@gmail.com>
2022-11-17 19:23:12 +0530

    my File

---
+++ src/person/person1.java
@@ -0,0 +1,56 @@
+package person;
+
+public class person1 {
+
+  public static void main(String[]args){
+
+   //Syntax:datatype var name =value;
+   int a= 5;
+
+   int cube=a*a*a;
+   System.out.println("cube is "+cube);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
---
+++ src/person/person2.java
@@ -0,0 +1,36 @@
+package person;
+
+public class person2{
+
+    public static void main(String[]args) {
+
+        //Syntax:datatype var name =value;
+        int a =10;
+        int d = 10;
+        double number =( a * d )/100;
+        System.out.println(" It is " + number);
+
+        int p =80;
+        int b =50;
+        double coin =(p*b)/100;
+        System.out.println("i have "+coin);
+
+int k =90;
+int e=60;
+int money = (k*e)/100;
+System.out.println("i have  money "+money);
+
+int l = 60;
+int x = 30;
+int rupees = (l*x)/100;
+System.out.println("i have rupees "+rupees);
+
+
+
+
+
+
+    }
+
+
+}
