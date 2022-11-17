332095b4d18b2dc15116b12b6672b61fe451b8d1
sandeep20198 <sandeepsingh20198@gmail.com>
2022-11-17 09:18:40 +0530

    MY Variable file

---
+++ src/variable/Percentage.java
@@ -0,0 +1,17 @@
+package variable;
+
+public class Percentage {
+    public static void main(String[] args) {
+         int a=98467;
+         int percrnt = 15;
+        int result = (a*percrnt/100);
+        System.out.println("15 percent of 98467 is = " + result);
+
+
+        int c=9;
+        int cube= c*c*c;
+        System.out.println("Cube of 9 is = " +cube);
+
+
+    }
+}
