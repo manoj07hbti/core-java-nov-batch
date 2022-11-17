5bcdb26b3cfb2d8c8ed1f3163ab464b47969a09f
aakas <iamakashg360@gmail.com>
2022-11-17 10:18:42 +0530

    this is commit for Day 7 Assignment

---
+++ src/variable/Day7Assignment.java
@@ -0,0 +1,18 @@
+package variable;
+
+public class Day7Assignment {
+
+    public static void main(String[] args) {
+
+        double a = 1922;
+        double per = 15;
+        double percent = (a*per)/100 ;
+
+        int s = 9;
+        int cube = s*s*s;
+
+        System.out.println("percentage of 1900 is - " +percent);
+        System.out.println("Cube of 12 is - " +cube);
+
+    }
+}
