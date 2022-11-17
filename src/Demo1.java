4cd010514544227727c0deb80fa8d844e071b323
Haider Ali <abbashaiderali76@gmail.com>
2022-11-17 06:57:57 -0800

    My Git commit

--- src/Demo1.java
+++ src/Demo1.java
@@ -1,2 +1,41 @@
 public class Demo1 {
+    public static void main(String [] args){
+        int a= 1;
+
+        a++; // a=a+1
+        System.out.println("value of a after a++" + a);
+
+        a--; // a=a-1
+        System.out.println("value of a after a--" + a);
+
+        int b=5;
+        int c=5;
+        int sum=b+c;
+        System.out.println("sum is " + sum);
+
+        int x=5;
+        int y=5;
+        int multiply=b*c;
+        System.out.println("multiply is " + multiply);
+
+        int w=5;
+        int z=5;
+        int divide=5/5;
+        System.out.println("divide is " + divide);
+
+        int number = 15;
+        int square = number*number;
+        System.out.println("the number is square " + square);
+
+        int Number = 12;
+        int cube = Number*Number*Number;
+        System.out.println(" the number is cube " + cube);
+
+
+
+
+    }
+
 }
+
+
