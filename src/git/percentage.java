b3dd462fc402493861df9a2247cf68e38b83ec45
Gaurang yadav <yadavgaurang11@gmail.com>
2022-11-17 16:15:53 +0530

    git testing

---
+++ src/git/percentage.java
@@ -0,0 +1,19 @@
+package git;
+
+public class percentage {
+    public static void main(String[] args){
+     int percentage=33;
+     int number=20000;
+    double result=(number*percentage)/100;
+     System.out.println(result);
+
+
+
+    }
+
+
+
+
+
+
+}
