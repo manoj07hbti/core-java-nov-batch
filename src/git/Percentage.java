e66f2d4ff64643016a3c6f80fc0204cd089d08f5
arun singh <rockstararun002@gmail.com>
2022-11-17 21:47:53 +0530

    percentage completed

---
+++ src/git/Percentage.java
@@ -0,0 +1,29 @@
+package git;
+
+public class Percentage {
+    public static void main(String[] args) {
+        int number=100;
+        int percentage=5;
+        double result=(number*percentage )/100;
+        System.out.println("percentage of 100 is "+result);
+        number=25;
+        percentage=5;
+        result=(double)(number*percentage)/100;
+        System.out.println("percentage of 25 is "+result);
+        number=636;
+        percentage=23;
+        result=(double)(number*percentage)/100;
+        System.out.println("percentage of 636 is "+result);
+        number=34343434;
+        percentage=34;
+        result=(double)(number*percentage)/100;
+        System.out.println(+result);
+        number=87834787;
+        percentage=34324;
+        result=(double)(number*percentage)/100;
+        System.out.println(+result);
+
+
+
+    }
+}
