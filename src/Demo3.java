61df81bf53018358f58518493ab7fed0badbc57f
Haider Ali <abbashaiderali76@gmail.com>
2022-11-17 07:06:56 -0800

    My Git third commit

---
+++ src/Demo3.java
@@ -0,0 +1,19 @@
+public class Demo3 {
+
+    public static void main(String [] args) {
+
+        // SYNTAX Datatype varName = value;
+        String Brand = "The coca cola company.";
+        System.out.println("The name of this company " + Brand);
+
+        // SYNTAX Datatype varName = value;
+        int employees = 5000;
+        System.out.println(employees + " people work in this company. ");
+
+        // SYNTAX Datatype varName = value ;
+        double sales = 1000000.50;
+        System.out.println("It sells " + sales + " bottle every year.");
+    }
+
+
+    }
