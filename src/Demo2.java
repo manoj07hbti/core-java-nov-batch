695a3396b4c8762854aed4153bb5e78ed2c8448c
Haider Ali <abbashaiderali76@gmail.com>
2022-11-17 07:02:04 -0800

    My Git second commit

---
+++ src/Demo2.java
@@ -0,0 +1,17 @@
+public class Demo2 {
+    public static void main(String[] args) {
+        // SYNTAX Datatype varName = value;
+        String name = "Salman Khan";
+        System.out.println("the bollywood best actor is " + name);
+
+        // SYNTAX Datatype varName = value;
+        int age = 57;
+        System.out.println("The age is " + age + " years ");
+
+        // SYNTAX Datatype value = value;
+        double income = 1000.50;
+        System.out.println("The income is " + income + " cr ");
+    }
+
+
+}
