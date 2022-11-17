6cce26c170b675c30bf5e246cce8bc1b83349b4c
arun singh <rockstararun002@gmail.com>
2022-11-17 09:23:46 +0530

    my cube class

--- src/git/Newjava.java
+++ src/git/Newjava.java
@@ -1,5 +1,50 @@
 package git;
 
 public class Newjava {
+    public static void main(String[] args) {
+        int number=2;
+        int cube=number*number*number;
+        System.out.println("cube of 2 is "+cube);
+        number=3;
+        cube=number*number*number;
+        System.out.println("cube of 3 is "+cube);
+        number=4;
+        cube=number*number*number;
+        System.out.println("cube of 4 is "+cube);
+        number=5;
+        cube=number*number*number;
+        System.out.println("cube of 5 is "+cube);
+        number=6;
+        cube=number*number*number;
+        System.out.println("cube of 6 is "+cube);
+        number=7;
+        cube=number*number*number;
+        System.out.println("cube of 7 is "+cube);
+        number=8;
+        cube=number*number*number;
+        System.out.println("cube of 8 is "+cube);
+        number=9;
+        cube=number*number*number;
+        System.out.println("cube of 9 is "+cube);
+        number=10;
+        cube=number*number*number;
+        System.out.println("cube of 10 is "+cube);
+        number=11;
+        cube=number*number*number;
+        System.out.println("cube of 11 is "+cube);
+        number=12;
+        cube=number*number*number;
+        System.out.println("cube of 12 is "+cube);
+        number=13;
+        cube=number*number*number;
+        System.out.println("cube of 13 is "+cube);
+        number=14;
+        cube=number*number*number;
+        System.out.println("cube of 14 is "+cube);
+        number=15;
+        cube=number*number*number;
+        System.out.println("cube of 15 is "+cube);
+
+    }
 
 }
