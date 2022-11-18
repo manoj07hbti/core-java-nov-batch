2c4317f05f12f8a65bda0fed68e04f592ee34d71
DEVESH KUMAR <Deep789516@gmail.com>
2022-11-18 16:33:19 +0530

    if  statments

---
+++ src/git/demo_if.java
@@ -0,0 +1,31 @@
+  package git;
+
+    public class demo_if {
+        public static void main(String[] args) {
+            int number = 65;
+            if (number > 60) {
+                System.out.println("First division");
+
+            }
+            if (number > 50) {
+                System.out.println("Second division");
+            }
+            if (number > 33) {
+                System.out.println("Third division");
+            }
+
+            int price = 1100;
+            if (price > 1000) {
+                System.out.println("Costly product.....");
+            }
+            if (price > 1000) {
+
+                System.out.println("Not costly product.....");
+            }
+
+        }
+
+    }
+
+
+
---
+++ src/git/if_else_statments.java
@@ -0,0 +1,22 @@
+package git;
+
+public class if_else_statments {
+
+public static void main(String[]args) {
+    int price = 1000;
+    if (price > 1500) {
+        System.out.println("It is so costly");
+    } else {
+        System.out.println("It not so much costly");
+    }
+    String city = "India";
+    if (city == "India") {
+        System.out.println("you belong to india");
+    } else {
+        System.out.println("You belongs to other city");
+    }
+
+    }
+
+
+}
