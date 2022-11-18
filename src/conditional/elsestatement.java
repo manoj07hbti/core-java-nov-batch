7956f7160ac14dd3d19c0798673c043d6eb0afb3
Gaurang yadav <yadavgaurang11@gmail.com>
2022-11-18 17:04:40 +0530

    git testing

---
+++ src/conditional/elsestatement.java
@@ -0,0 +1,45 @@
+package conditional;
+
+public class elsestatement {
+    public static void main(String[] args){
+      int price=20000;
+      if(price>10000){
+          System.out.println("this is costly product");
+          }
+      else {
+          System.out.println("this is not costly product");
+
+
+
+      }
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
