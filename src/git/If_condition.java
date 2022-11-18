574e6adc57181fe71fa6d35e8207da723e260d58
SIDDHARTHSSA <WILLLLBEDONE@GMAIL.COM>
2022-11-19 00:58:17 +0530

    first if condition program

---
+++ src/git/If_condition.java
@@ -0,0 +1,18 @@
+package git;
+
+public class If_condition {
+    public static void main(String[] args) {
+        int percentage=65,55,40;
+        if ( percentage>=60 ) {
+            System.out.println("the child got first division");
+        }
+
+        if ( percentage>=50 ){
+            System.out.println("the child got second division");
+        }
+if ( percentage>=33 ){
+    System.out.println("the child got third division");
+
+        }
+    }
+}
