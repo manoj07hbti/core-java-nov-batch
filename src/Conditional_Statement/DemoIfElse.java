804244f838da42edab8eedfc46e29e6b51bc71bd
VIN07-27 <vs1114466@gmail.com>
2022-11-18 14:54:34 +0530

    Conditional-Statement-day-1

---
+++ src/Conditional_Statement/DemoIfElse.java
@@ -0,0 +1,24 @@
+package Conditional_Statement;
+
+public class DemoIfElse {
+
+    public static void main(String[] args) {
+
+        int age = 17;
+        if (age > 18) {
+
+            System.out.println("Eligible for voting...");
+        } else {
+            System.out.println("Not eligible for voting...");
+        }
+
+        String city = "Delhi";
+        if (city== "AGRA"){
+        System.out.println("You Belongs to AGRA...");
+    }
+        else{
+            System.out.println("You Belongs to another city...");
+        }
+
+    }
+}
