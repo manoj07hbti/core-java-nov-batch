5cde2f4b5ca15faec74e9b7b3ff51f78c9662707
Gaurang yadav <yadavgaurang11@gmail.com>
2022-11-18 16:57:53 +0530

    git testing

---
+++ src/conditional/Statement.java
@@ -0,0 +1,56 @@
+package conditional;
+
+public class Statement{
+    public static void main(String[] args){
+        int age=15;
+        if(age>18){
+            System.out.println("eligible for voting");
+
+        }
+        else{
+          System.out.println("not eligibilty for voting");
+
+        }
+        int marks=80;
+        if(marks>60){
+            System.out.println("first division");
+        if (marks>50){
+            System.out.println("second division");
+         if (marks>33){
+             System.out.println("third division");
+             String city="agra";
+             if(city=="agra") {
+                 System.out.println("this is my hometown");
+             }
+             else{
+                 System.out.println("This is not my hometown");
+
+
+             }
+
+
+
+         }
+        }
+
+
+
+
+        }
+
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+}
