7b8f4ff7a01e55f8c927fc972768e8118355d0a7
aakas <iamakashg360@gmail.com>
2022-11-18 09:53:49 +0530

    Marks Conditional Program Commit

---
+++ src/variable/Day8Assignment_if.java
@@ -0,0 +1,30 @@
+package variable;
+
+public class Day8Assignment_if {
+
+    /*  Conditional Statement Syntax
+            if (Condition){
+                Write the code
+                }
+        Condition If marks  >60   first division
+                            >50   second
+                            >33   third
+
+         */
+
+    public static void main(String[] args) {
+        int marks = 70;
+        if (marks>60){
+            System.out.println("First Division");
+        }
+        int marks2 = 52;
+        if (marks2>50){
+            System.out.println("Second Division");
+        }
+        int marks3 = 52;
+        if (marks3>50){
+            System.out.println("Second Division");
+        }
+    }
+
+}
