cd547086bdbbd088f188b9b36110be0a7dbaa1d5
arun singh <rockstararun002@gmail.com>
2022-11-18 12:44:47 +0530

    if else condition

---
+++ src/git/Elseclass.java
@@ -0,0 +1,35 @@
+package git;
+
+public class Elseclass {
+    public static void main(String[] args) {
+        String country="italy";
+        if(country=="india"){
+            System.out.println("person belong to india..");
+        }
+        else{
+            System.out.println("belong to other country..");
+        }
+        country="india";
+        if(country=="india"){
+            System.out.println("belong to india");
+        }
+        else{
+            System.out.println("belong to other country");
+        }
+        int price=900;
+        if(price>1000){
+            System.out.println("price is good");
+        }
+        else{
+            System.out.println("price is bad");
+        }
+        int marks=62;
+        if(marks>60){
+            System.out.println("first devision");
+        }
+        else{
+            System.out.println("marks is second and third devision");
+        }
+
+    }
+}
