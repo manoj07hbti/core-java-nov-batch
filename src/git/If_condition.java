b0907ed361dc9d32689a3e744b30899b19cc92ec
SIDDHARTHSSA <WILLLLBEDONE@GMAIL.COM>
2022-11-19 08:16:57 +0530

    first if condition program

--- src/git/If_condition.java
+++ src/git/If_condition.java
@@ -2,7 +2,7 @@ package git;
 
 public class If_condition {
     public static void main(String[] args) {
-        int percentage=65,55,40;
+        int percentage=65;
         if ( percentage>=60 ) {
             System.out.println("the child got first division");
         }
@@ -10,6 +10,7 @@ public class If_condition {
         if ( percentage>=50 ){
             System.out.println("the child got second division");
         }
+
 if ( percentage>=33 ){
     System.out.println("the child got third division");
 
---
+++ src/ifcondition2/Price.java
@@ -0,0 +1,14 @@
+package ifcondition2;
+
+public class Price {
+    public static void main(String[] args) {
+        int cost = 1500;
+        if ( cost >1000) {
+            System.out.println("the commodity is costly");
+        }
+            if ( cost < 1000) {
+                System.out.println("the commodity is cheaper");
+
+        }
+    }
+}
