54ea020d1e2a731415241a9900d376f5ac2e55fa
sandeep20198 <sandeepsingh20198@gmail.com>
2022-11-19 17:26:49 +0530

    If & Else File(COUNTRY)

---
+++ src/conditional_statement/country.java
@@ -0,0 +1,17 @@
+package conditional_statement;
+
+public class country {
+    public static void main(String[] args) {
+        String Country= "india";
+
+        if (Country == "india"){
+            System.out.println("Belongs to india");
+        }else{
+            System.out.println("not Belongs to india");
+        }
+
+
+
+    }
+}
+
