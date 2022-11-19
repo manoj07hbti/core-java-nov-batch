cbf51c270a4fa6d2745f77a45351cd8f3c2c0365
feature-varun <pratapvarun21@gmail.com>
2022-11-19 08:43:28 +0530

    if-marks

--- src/conditionalStatement/Ifstatement.java
+++ src/conditionalStatement/Ifstatement.java
@@ -4,14 +4,14 @@ public class Ifstatement {
 
     public static void main(String[] args) {
 
-        int mark = 39;
+        int mark = 55;
         if (mark >= 60) {
             System.out.println("the candidate pass the exam by 1st division");
         }
-        if (mark >= 40) {
+        if (mark >= 50) {
             System.out.println("the candidate pass the exam bu 2nd division");
         }
-        if (mark <= 40) {
+        if (mark <= 50) {
             System.out.println("the candidate pass the exam with 3rd division");
 
         }
