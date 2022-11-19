1b1a2b804838bb25cd95280e9de3ed3310bbbaa2
sandeep20198 <sandeepsingh20198@gmail.com>
2022-11-19 11:18:56 +0530

    If & Else File

---
+++ src/conditional_statement/ifrlse.java
@@ -0,0 +1,21 @@
+package conditional_statement;
+
+public class ifrlse {
+    public static void main(String[] args) {
+
+        int age=19;
+        if (age>=18) {
+            System.out.println("adult");
+        }else {
+            System.out.println("not adult");
+        }
+
+
+        int a=17;
+        if (a/2==0){
+            System.out.println("even");
+        }else{
+            System.out.println("odd");
+        }
+    }
+}
