f74b403a7821136979c96d678e150725760ed7af
feature-varun <pratapvarun21@gmail.com>
2022-11-20 22:15:11 +0530

    modulusoperator

---
+++ src/conditionalStatement/modulusoperator.java
@@ -0,0 +1,20 @@
+package conditionalStatement;
+
+public class modulusoperator {
+
+    public static void main(String[]args){
+
+        int number=1;
+        if(number%2==0){
+            System.out.println("the number is even");
+
+        }
+        if(number%2==1){
+            System.out.println("the number is odd");
+        }
+        else{
+            System.out.println("you enter the negative vale");
+        }
+    }
+
+}
