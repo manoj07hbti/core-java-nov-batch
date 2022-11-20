77686e641fa312ed74d0da628178adf6dc8c5bbf
arun singh <rockstararun002@gmail.com>
2022-11-20 20:53:50 +0530

    moduler

---
+++ src/Packagenov/Packagenov.java
@@ -0,0 +1,8 @@
+package Packagenov;
+
+public class Packagenov {
+    public static void main(String[] args) {
+        String city="agra";
+        System.out.println("my city name is "+city);
+    }
+}
