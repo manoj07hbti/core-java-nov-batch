cd130ccd4828628e482a9d8cdb828250cb280201
arun singh <rockstararun002@gmail.com>
2022-11-23 10:34:26 +0530

    even odd number upto 100

---
+++ src/git/Oddeven.java
@@ -0,0 +1,15 @@
+package git;
+
+public class Oddeven {
+    public static void main(String[] args) {
+
+        for(int i=0;i<=100;i++){
+            if(i%2==0){
+                System.out.println("even nuber "+i);
+            }
+            else{
+                System.out.println("odd number "+i);
+            }
+        }
+    }
+}
