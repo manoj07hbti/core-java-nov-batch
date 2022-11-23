869691c57c57411a8753882ceac305ef4e9adf80
Gaurang yadav <yadavgaurang11@gmail.com>
2022-11-23 16:58:44 +0530

    loop assignment

---
+++ src/conditional/Loopassignment.java
@@ -0,0 +1,23 @@
+package conditional;
+
+public class Loopassignment {
+    public static void main(String [] args){
+     int num=5;
+     int result;
+     for(int i=1; i<=10; i++){
+         result=num*i;
+         System.out.println(num +"*"+i+"="+ result);
+
+     }
+     for(int i=1; i<=100; i++){
+        if(i%2==0){
+            System.out.println(i +"is"  +"even");
+        } else{
+            System.out.println(i +"is" +"odd");
+        }
+
+
+     }
+
+    }
+}
---
+++ src/conditional/loop.java
@@ -0,0 +1,13 @@
+package conditional;
+
+public class loop {
+    public static void main(String [] args) {
+        for (int i = 0; i < 5; i++) {
+            System.out.println("hello world" + i);
+        }
+
+     for(int i=10; i>0; i--){
+         System.out.println("hello world" +i);
+     }
+    }
+}
