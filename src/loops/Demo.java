65feff309536332a7edd182ace8c1cf18470b625
60manojkumar@gmail.com <Motivated@2019>
2022-11-23 08:47:15 +0530

    for Loop

---
+++ src/loops/Demo.java
@@ -0,0 +1,23 @@
+package loops;
+
+public class Demo {
+
+    public static void main(String[] args) {
+
+
+        //increment for loop
+
+        for(int i=0; i<5; i++ ){
+
+            System.out.println("Hello World "+i);
+        }
+
+        //decrement for loop
+
+        for (int i=10; i>0; i--){
+
+            System.out.println("Decrement Loop : "+i);
+        }
+
+    }
+}
---
+++ src/loops/TableofTwo.java
@@ -0,0 +1,16 @@
+package loops;
+
+public class TableofTwo {
+    public static void main(String[] args) {
+
+        int number=2;
+        int result;
+
+        for(int i=1; i<=10; i++){
+
+            result=number*i;
+            System.out.println(number + "X" +i+ " = " +result);
+        }
+
+    }
+}
