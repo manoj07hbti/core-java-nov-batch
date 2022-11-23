94013848ae76cc7e566f40217a3b1a335c156683
feature-varun <pratapvarun21@gmail.com>
2022-11-23 09:37:03 +0530

    table5

---
+++ src/loops/Table5.java
@@ -0,0 +1,16 @@
+package loops;
+
+public class Table5 {
+    public static void main(String[]args){
+        int number=5;
+        int result;
+
+        for(int j=1; j<=10; j++){
+
+            result=number*j;
+            System.out.println(number + "X" +j+ " = " +result);
+        }
+
+    }
+}
+
