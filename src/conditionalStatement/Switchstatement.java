b31867d63f75bc014f04efb888ae1f4a493a6e18
feature-varun <pratapvarun21@gmail.com>
2022-11-23 09:11:36 +0530

    switchstatement

---
+++ src/conditionalStatement/Switchstatement.java
@@ -0,0 +1,50 @@
+package conditionalStatement;
+
+public class Switchstatement {
+
+    public static void main(String[] args) {
+        int month = 3;
+        switch (month) {
+            case 1:
+                System.out.println(" You choose the month January"+month);
+                break;
+            case 2:
+                System.out.println(" You choose the month February"+month);
+                break;
+            case 3:
+                System.out.println(" You choose the month March"+month);
+                break;
+            case 4:
+                System.out.println(" You choose the month April"+month);
+                break;
+            case 5:
+                System.out.println(" You choose the month May"+month);
+                break;
+            case 6:
+                System.out.println(" You choose the month June"+month);
+                break;
+            case 7:
+                System.out.println(" You choose the month July"+month);
+                break;
+            case 8:
+                System.out.println(" You choose the month August"+month);
+                break;
+            case 9:
+                System.out.println(" You choose the month September"+month);
+                break;
+            case 10:
+                System.out.println(" You choose the month October"+month);
+                break;
+            case 11:
+                System.out.println(" You choose the month November"+month);
+                break;
+            case 12:
+                System.out.println(" You choose the month December"+month);
+                break;
+            default:System.out.println("you enter the wrong month number");
+
+
+
+        }
+    }
+}
