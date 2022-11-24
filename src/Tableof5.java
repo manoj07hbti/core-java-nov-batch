2410864c8d7c473d06ea54233bc078c94636409c
SIDDHARTHSSA <WILLLLBEDONE@GMAIL.COM>
2022-11-24 16:03:57 +0530

    first if condition program

---
+++ src/Tableof5.java
@@ -0,0 +1,12 @@
+public class Tableof5 {
+    public static void main(String[] args) {
+        int number = 5;
+        int result;
+        for (int i=1; i<=20; i++){
+            result=number*i;
+            System.out.println(number+"X" +i+ "=" +result );
+
+        }
+
+    }
+}
