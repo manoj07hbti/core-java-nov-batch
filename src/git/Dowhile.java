c51e4e72095b399922714b65aadaf19e6310ac86
arun singh <rockstararun002@gmail.com>
2022-11-24 22:00:06 +0530

    whilecondition

--- src/git/Dowhile.java
+++ src/git/Dowhile.java
@@ -2,14 +2,53 @@ package git;
 
 public class Dowhile {
     public static void main(String[] args) {
-        int i=0;
-
+        int number=5;
+        int i=1;
+        int result;
         do{
-            System.out.println("this is while loop "+i);
+            result=number*i;
+            System.out.println(number+"*"+i+"=" +result);
+                 i++;
+        }
+        while(i<=10);
+        number=9;
+        i=1;
+        do{
+            result=number*i;
+            System.out.println(number+"*"+i+"=" +result);
             i++;
         }
-        while(i<=5);
+        while(i<=10);
 
 
+        //odd even
+
+        do {
+            if (i % 2 == 0) {
+                System.out.println("even number " + i);
+
+            } else {
+                System.out.println("odd number " + i);
+            }
+            i++;
+        }
+        while(i<=100);
+
+        // factorial
+        i=5;
+        int fact=1;
+        do{
+            fact=fact*i;
+            i--;
+            System.out.println("fact of 5 is "+fact);
+        }
+        while(i>0);
+
+
+
+
+
+    }
+
 }
-}
+
