8b1919e3a2700558fce6ec6f1c33819ea36ab4c8
Gaurang yadav <yadavgaurang11@gmail.com>
2022-11-24 08:49:58 +0530

    factorial

---
+++ src/conditional/factorial.java
@@ -0,0 +1,16 @@
+package conditional;
+
+public class factorial {
+    public static void main(String[] args){
+       int num=5;
+       int factorial=1;
+       for(int i=5; i>0; i--){
+           factorial=factorial *i;
+
+       }
+        System.out.println("factorial of 5 is"+factorial );
+
+
+    }
+
+}
