1d71aa91acd53d43af87d5131b4d46b1a5bbdd8c
SIDDHARTHSSA <WILLLLBEDONE@GMAIL.COM>
2022-11-24 17:07:38 +0530

    dowhile loop program

---
+++ src/switc_case/Dowhileloop.java
@@ -0,0 +1,15 @@
+package switc_case;
+
+public class Dowhileloop {
+    public static void main(String[] args) {
+
+        int i = 0;
+        do {
+            System.out.println("I love to play games" + i);
+            i++;
+
+        }
+        while (i <= 40);
+
+    }
+}
---
+++ src/switc_case/While_loop.java
@@ -0,0 +1,14 @@
+package switc_case;
+
+public class While_loop {
+    public static void main(String[] args) {
+
+        int i = 0;
+        while (i <= 50)
+        {
+            System.out.println("I love to play games" + i);
+            i++;
+
+        }
+    }
+}
