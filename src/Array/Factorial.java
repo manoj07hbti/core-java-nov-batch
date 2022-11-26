8814087890bdf97ea1201785cdd717a4dd27775e
DEVESH KUMAR <Deep789516@gmail.com>
2022-11-26 16:57:43 +0530

    Factorial

--- src/Array/Factorial.java
+++ src/Array/Factorial.java
@@ -4,10 +4,29 @@ public class Factorial {
     public static void main(String[] args) {
         int number = 5;
         int factorial = 1;
-        for (int i=number; i>0; i--) {
+        for (int i = number; i > 0; i--) {
             factorial = factorial * i;
         }
-            System.out.println("Factorial is "+factorial);
+        System.out.println("Factorial is " + factorial);
 
+        int digit = 6;
+        factorial = 1;
+
+        for (int i = digit; i > 0; i--) {
+            factorial = factorial * i;
+        }
+
+        System.out.println("Factorial is " + factorial);
+
+        int num = 7;
+        factorial = 1;
+
+        for (int i = num; i > 0; i--) {
+            factorial = factorial * i;
+        }
+
+            System.out.println("Factorial is " + factorial);
+
+        }
     }
-}
+
