8823f0566eb7d65cb0b45092e417e7e00f5224fe
arun singh <rockstararun002@gmail.com>
2022-11-28 10:49:35 +0530

    building

---
+++ src/git/Building.java
@@ -0,0 +1,14 @@
+package git;
+
+public class Building {
+    String building="burj khalifa";
+    int floor=198;
+    int flat=1009;
+
+    public static void main(String[] args) {
+        Building obj = new Building();
+
+        System.out.println("building name is: "+obj.building + " no.of flat: "+obj.flat + " no.of floor: "+obj.floor );
+
+    }
+}
