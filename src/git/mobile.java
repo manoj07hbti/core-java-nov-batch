e48f8abe4f5750e5b8cdc420577df81024ddd354
arun singh <rockstararun002@gmail.com>
2022-11-28 10:28:17 +0530

    advance loop

---
+++ src/git/mobile.java
@@ -0,0 +1,62 @@
+package git;
+
+public class mobile {
+    String company = "apple";
+    int price = 100000;
+    int ram = 8;
+
+    public static void main(String[] args) {
+        mobile obj = new mobile();
+        System.out.println("company:" + obj.company + "price:" + obj.price + "ram: " + obj.ram);
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
