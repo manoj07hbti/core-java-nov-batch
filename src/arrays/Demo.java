c0a5d2067bb7488a8c502d08c8cbad2f8d52f301
60manojkumar@gmail.com <Motivated@2019>
2022-11-30 08:46:24 +0530

    Array

---
+++ src/arrays/Demo.java
@@ -0,0 +1,21 @@
+package arrays;
+
+public class Demo {
+
+    public static void main(String[] args) {
+
+        String array[]={ "Mobile :10,000" ,"Laptop : 40,000", "Pen: 25 ", "Car: 5,00000"};
+
+
+        for (String var: array){
+
+            System.out.println(var);
+        }
+
+        double price[]= {2134.4, 543.4, 32.5,343.55};
+
+        String product[]= { "mobile","laptop","pen","car"};
+
+
+    }
+}
