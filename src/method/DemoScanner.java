eb55df8d9010e229a9ee23e4b15c6d46a10cb6fe
Haider Ali <abbashaiderali76@gmail.com>
2022-12-05 18:37:04 -0800

    Method All Class Assignment

---
+++ src/method/DemoScanner.java
@@ -0,0 +1,34 @@
+package method;
+
+import java.util.Scanner;
+
+public class DemoScanner {
+
+    public static void main(String[] args) {
+
+        // Create object of Scanner
+
+        Scanner scanner = new Scanner(System.in);
+
+        // Reading data with String
+        System.out.println("Please enter your country name......" );
+        String name= scanner.nextLine();
+        System.out.println("You have entered your country name " + name);
+
+        // Reading data with int
+        System.out.println("Please enter your state number...." );
+        int state= scanner.nextInt();
+        System.out.println("You have entered your state " + state);
+
+        // Reading data with long
+        System.out.println("Please enter your total population.....");
+        long population=scanner.nextLong();
+        System.out.println("You have entered your country population..." +population);
+
+
+
+
+
+    }
+
+}
