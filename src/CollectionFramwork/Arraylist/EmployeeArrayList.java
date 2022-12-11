4ed50d4d4450ea0f73a14f7424fcca02ae67441b
DEVESH KUMAR <Deep789516@gmail.com>
2022-12-11 21:12:41 +0530

    ModelArraylist Assignment

---
+++ src/CollectionFramwork/Arraylist/EmployeeArrayList.java
@@ -0,0 +1,42 @@
+package CollectionFramwork.Arraylist;
+
+import CollectionFramwork.Model.Employee;
+
+import java.util.ArrayList;
+
+public class EmployeeArrayList {
+
+    public ArrayList<Employee> createEmployeeList() {
+
+        // SYNTAX :  ArrayList <Datatype> objName= new ArrayList();
+        ArrayList<Employee> Employeee = new ArrayList<>();
+// create Employee data
+        Employee employee1 = new Employee("Raghav", 66000, "Microsoft");
+
+        Employee employee2 = new Employee("Herry", 200000, "Google");
+
+        Employee employee3 = new Employee("Harshit", 210000, "Apple");
+
+        Employeee.add(employee1);
+        Employeee.add(employee2);
+        Employeee.add(employee3);
+        return Employeee;
+    }
+
+
+    public static void main(String[] args) {
+
+        EmployeeArrayList obj = new EmployeeArrayList();
+
+        ArrayList<Employee> EmployeeeList = obj.createEmployeeList();
+
+            for (int i = 0; i < EmployeeeList.size(); i++) {
+
+            System.out.println("Employee name " + EmployeeeList.get(i).getName());
+            System.out.println("Employee salary " + EmployeeeList.get(i).getSalary());
+            System.out.println("Employee Company " + EmployeeeList.get(i).getCompany());
+
+
+        }
+    }
+}
---
+++ src/CollectionFramwork/Model/Employee.java
@@ -0,0 +1,47 @@
+package CollectionFramwork.Model;
+
+public class Employee {
+    String name;
+    int salary;
+    String company;
+
+    // by use of  parameter constructor
+
+
+    public Employee(String name, int salary, String company) {
+        this.name = name;
+        this.salary = salary;
+        this.company = company;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+
+        this.name = name;
+    }
+
+    public int getSalary() {
+
+        return salary;
+    }
+
+    public void setSalary(int salary) {
+
+        this.salary = salary;
+    }
+
+    public String getCompany() {
+        return company;
+    }
+
+    public void setCompany(String company) {
+        this.company = company;
+    }
+}
+
+
+
+
