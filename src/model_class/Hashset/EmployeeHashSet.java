db04b558c50a6ad3b66791e48eea8e9181560322
Haider Ali <abbashaiderali76@gmail.com>
2022-12-16 04:02:24 -0800

    HashSet with Hashcode

--- src/model_class/Hashset/EmployeeHashSet.java
+++ src/model_class/Hashset/EmployeeHashSet.java
@@ -16,13 +16,29 @@ public class EmployeeHashSet {
         // create Employee Data
 
         Employee employee1 = new Employee("Sonu kumar", 35, 35000.35);
+        System.out.println("Hashcode of Employee 1 : " +employee1.hashCode());
+
         Employee employee2 = new Employee("Ravi kumar", 45, 45000.45);
+        System.out.println("Hashcode of Employee 2 : " +employee2.hashCode());
+
         Employee employee3 = new Employee("Rahul kumar", 40, 40000.40);
+        System.out.println("Hashcode of Employee 3 : " +employee3.hashCode());
+
         Employee employee4 = new Employee("Mohit kumar", 30, 30000.30);
+        System.out.println("Hashcode of Employee 4 : " +employee4.hashCode());
+
         Employee employee5 = new Employee("Raaj kumar", 25, 25000.25);
+        System.out.println("Hashcode of Employee 5 : " +employee5.hashCode());
+
         Employee employee6 = new Employee("Sonu kumar", 35, 35000.35);
+        System.out.println("Hashcode of Employee 6 : " +employee6.hashCode());
+
         Employee employee7 = new Employee("Ravi kumar", 45, 45000.45);
+        System.out.println("Hashcode of Employee 7 : " +employee7.hashCode());
+
         Employee employee8 = new Employee("Rahul kumar", 40, 40000.40);
+        System.out.println("Hashcode of Employee 8 : " +employee8.hashCode());
+
 
         employeeHashSet.add(employee1);
         employeeHashSet.add(employee2);
@@ -42,7 +58,7 @@ public class EmployeeHashSet {
         HashSet<Employee> employeeHashSet = obj.createEmployeeHashSet();
 
         for (Employee var : employeeHashSet){
-            System.out.println("Printing all data : " +var.getName() + "Age : " +var.getAge() + "Salary : " +var.getSalary());
+            System.out.println("Printing all data :: " +var.getName() + " Age : "  +var.getAge() + " Salary : "  +var.getSalary());
         }
 
     }
