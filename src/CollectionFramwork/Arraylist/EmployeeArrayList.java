683a832461823a603b9df45a4fd9e05c11bda9c1
DEVESH KUMAR <Deep789516@gmail.com>
2022-12-16 22:04:01 +0530

    EmployeeArrayList

--- src/CollectionFramwork/Arraylist/EmployeeArrayList.java
+++ src/CollectionFramwork/Arraylist/EmployeeArrayList.java
@@ -1,41 +1,43 @@
-package CollectionFramwork.Arraylist;
+package CollectionFramwork.ArrayList;
 
-import CollectionFramwork.Arraylist.Model.Employee;
+import CollectionFramwork.Model.Employee;
 
 import java.util.ArrayList;
 
 public class EmployeeArrayList {
+    public EmployeeArrayList() {
+    }
 
     public ArrayList<Employee> createEmployeeList() {
 
-        // SYNTAX :  ArrayList <Datatype> objName= new ArrayList();
-        ArrayList<Employee> Employeee = new ArrayList<>();
-// create Employee data
+        // SYNTAX :  ArrayList <Datatype> objName = new ArrayList();
+
+        ArrayList<Employee> Employeee = new ArrayList<>()
+                ;
+        // create Employee data
+
         Employee employee1 = new Employee("Raghav", 66000, "Microsoft");
-
         Employee employee2 = new Employee("Herry", 200000, "Google");
-
         Employee employee3 = new Employee("Harshit", 210000, "Apple");
-
         Employeee.add(employee1);
         Employeee.add(employee2);
         Employeee.add(employee3);
         return Employeee;
     }
-
-
     public static void main(String[] args) {
 
         EmployeeArrayList obj = new EmployeeArrayList();
-
         ArrayList<Employee> EmployeeeList = obj.createEmployeeList();
 
-            for (int i = 0; i < EmployeeeList.size(); i++) {
+        for (int i = 0; i < EmployeeeList.size(); i++) {
 
             System.out.println("Employee name " + EmployeeeList.get(i).getName());
             System.out.println("Employee salary " + EmployeeeList.get(i).getSalary());
             System.out.println("Employee Company " + EmployeeeList.get(i).getCompany());
 
+   // When we want to print in one line
+
+            System.out.println("Employee name : "+ EmployeeeList.get(i).getName() + " Employee salary : " + EmployeeeList.get(i).getSalary() +"Employee company : "+ EmployeeeList.get(i).getCompany());
 
         }
     }
