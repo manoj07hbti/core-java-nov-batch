1b1074f070cf27cd4fb4846e36c7388777a61b19
Haider Ali <abbashaiderali76@gmail.com>
2022-12-26 04:26:54 -0800

    Exception Handling

---
+++ src/exception/Calculator.java
@@ -0,0 +1,42 @@
+package exception;
+
+public class Calculator {
+
+    public int divide (int a, int b){
+
+        System.out.println("Divide method is started : ");
+
+        int result = 0;
+
+        try {
+            result =a/b;
+        }
+        catch (Exception e){
+            System.out.println("Exception occurred is here : ");
+        }
+
+        System.out.println("Result is calculated : " + result);
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+
+        Calculator obj = new Calculator();
+        System.out.println("object is created : ");
+        int result = obj.divide(10,0);
+
+        System.out.println("Divide is here : " +result);
+
+        String arr [] = {"java" , "java8" , "Spring" , "Spring Boot"};
+
+        try {
+            System.out.println(arr [5]);
+        }
+        catch (Exception e ){
+            System.out.println(e);
+        }
+
+        System.out.println("Array printed : ");
+    }
+}
